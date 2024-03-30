03c997c6e0cbb56de0fe1e2f3b3b5f9ba60b79b7
etenesaca <edgartenesaca@gmail.com>
2024-03-29 23:50:51 -0500

    Set def folders

--- src/extension.ts
+++ src/extension.ts
@@ -4,32 +4,130 @@ import * as vscode from 'vscode';
 import * as child_process from 'child_process';
 import * as path from 'path';
 
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
+function call_odoo_scafold(
+	mdl: string,
+	mdl_path: string,
+	odoo_bin_folder: string,
+	python_bin_path: string
+): Promise<void> {
+	return new Promise((resolve, reject) => {
+		// Ruta completa del script Python que deseas ejecutar
+		const scriptPython = path.join(odoo_bin_folder, `odoo-bin scaffold ${mdl} ${mdl_path}`);
 
-function call_odoo_scafold(mdl: string, mdl_path: string): void {
-	// Ruta completa del script Python que deseas ejecutar
-	let python_bin = '/home/edgar/venvs/3.10/bin/python';
-	let odoo_bin = '/home/edgar/Git/odoo/';
-	const scriptPython = path.join(odoo_bin, `odoo-bin scaffold ${mdl} ${mdl_path}`);
-
-	// Ejecutar el script Python
-	child_process.exec(`${python_bin} ${scriptPython}`, (error, stdout, stderr) => {
-		if (error) {
-			vscode.window.showErrorMessage(`Error al ejecutar el script Python: ${error.message}`);
-			return;
-		}
-		if (stderr) {
-			vscode.window.showErrorMessage(`Error al ejecutar el script Python: ${stderr}`);
-			return;
-		}
-		vscode.window.showInformationMessage(`Script Python ejecutado correctamente. Salida: ${stdout}`);
+		// Ejecutar el script Python
+		child_process.exec(`${python_bin_path}/python ${scriptPython}`, (error, stdout, stderr) => {
+			if (error) {
+				vscode.window.showErrorMessage(`Error al ejecutar Odoo scafold: ${error.message}`);
+				reject(error);
+			}
+			if (stderr) {
+				vscode.window.showErrorMessage(`Error al ejecutar Odoo scafold: ${stderr}`);
+				reject(stderr);
+			}
+			vscode.window.showInformationMessage(`Módulo creado "${mdl}" `);
+			resolve();
+		});
 	});
 
 }
 
+function get_modules_path(): Promise<string> {
+	return new Promise((resolve, reject) => {
+		const config = vscode.workspace.getConfiguration('odoo-awesome');
+		const key = 'modules_directory';
+		const def_path = config.get(key);
+		const options: vscode.OpenDialogOptions = {
+			openLabel: 'Folder to save module',
+			canSelectMany: false, canSelectFiles: false, canSelectFolders: true,
+			defaultUri: def_path !== '' ? vscode.Uri.file(def_path + '') : undefined
+		};
+		vscode.window.showOpenDialog(options).then(resUri => {
+			if (resUri && resUri[0]) {
+				let res_path: string = resUri[0].fsPath + '';
+				// Configurar la carpeta seleccionda comp carpeta por defecto
+				config.update(key, res_path, vscode.ConfigurationTarget.Global)
+					.then(() => {
+						console.log('Modules folder default updated!!');
+						resolve(res_path);
+					});
+			} else {
+				reject('x');
+			}
+		});
+	});
+}
 
+function get_odoo_bin_folder(): Promise<string> {
+	return new Promise((resolve, reject) => {
+		const config = vscode.workspace.getConfiguration('odoo-awesome');
+		const key = 'odoo-bin-path';
+		const res = config.get(key);
+		if (res != '') {
+			resolve(res + '');
+		} else {
+			const options: vscode.OpenDialogOptions = {
+				openLabel: 'Select ODOO BIN folder',
+				canSelectMany: false, canSelectFiles: false, canSelectFolders: true
+			};
+			vscode.window.showOpenDialog(options).then(resUri => {
+				if (resUri && resUri[0]) {
+					let res_path = resUri[0].fsPath + '';
+					config.update(key, res_path, vscode.ConfigurationTarget.Global)
+						.then(() => {
+							console.log('Odoo folder updated!!');
+							resolve(res_path);
+						});
+				} else {
+					reject();
+				}
+			});
+		}
+	});
+}
+
+function get_python_bin_folder(): Promise<string> {
+	return new Promise((resolve, reject) => {
+		const config = vscode.workspace.getConfiguration('odoo-awesome');
+		const key = 'python-bin-folder';
+		const res = config.get(key);
+		if (res != '') {
+			resolve(res + '');
+		} else {
+			const options: vscode.OpenDialogOptions = {
+				openLabel: 'Select PYTHON BIN folder',
+				canSelectMany: false, canSelectFiles: false, canSelectFolders: true
+			};
+			vscode.window.showOpenDialog(options).then(resUri => {
+				if (resUri && resUri[0]) {
+					let res_path = resUri[0].fsPath + '';
+					config.update(key, res_path, vscode.ConfigurationTarget.Global)
+						.then(() => {
+							console.log('PYTHON folder updated!!');
+							resolve(res_path);
+						});
+				} else {
+					reject();
+				}
+			});
+		}
+	});
+}
+
+
+// This method is called when your extension is activated
+// Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
+
+	const config = vscode.workspace.getConfiguration('odoo-awesome');
+
+	// También puedes escuchar cambios en las preferencias
+	const disposables = vscode.workspace.onDidChangeConfiguration(event => {
+		if (event.affectsConfiguration('odoo-awesome.odoo-bin-path')) {
+			const newValue = config.get('odoo-bin-path');
+			console.log('La preferencia odoo-awesome.odoo-bin-path ha cambiado:', newValue);
+			vscode.window.showErrorMessage("Creación del módulo cancelada.");
+		}
+	});
 
 	// Use the console to output diagnostic information (console.log) and errors (console.error)
 	// This line of code will only be executed once when your extension is activated
@@ -39,34 +137,34 @@ export function activate(context: vscode.ExtensionContext) {
 	// Now provide the implementation of the command with registerCommand
 	// The commandId parameter must match the command field in package.json
 	let disposable = vscode.commands.registerCommand('odoo-new-module', () => {
-		let valorUsuario: string | undefined;
-
+		let newModuleName: string | undefined;
 		const inputBox = vscode.window.createInputBox();
 		inputBox.title = "Crear nuevo módulo de odoo";
 		inputBox.placeholder = "Nombre del módulo";
 		inputBox.onDidChangeValue(value => {
-			valorUsuario = value;
+			newModuleName = value;
 		});
 		inputBox.onDidAccept(() => {
-			if (valorUsuario) {
-
-				const options: vscode.OpenDialogOptions = {
-					canSelectMany: false,
-					openLabel: 'Select',
-					canSelectFiles: false,
-					canSelectFolders: true
-				};
-
-				vscode.window.showOpenDialog(options).then(fileUri => {
-					
-					if (fileUri && fileUri[0]) {
-						let xPath = fileUri[0].fsPath;
-						call_odoo_scafold(valorUsuario + '', xPath);
-						//vscode.window.showInformationMessage(`Nuevo módulo a crear: ${xPath}`);
-					}
-				});
-
-
+			if (newModuleName) {
+				let mdl_path: string;
+				let odoo_bin_path: string;
+				let python_bin_path: string;
+				get_modules_path()
+					.then(path => {
+						mdl_path = path;
+						return get_odoo_bin_folder();
+					})
+					.then(path => {
+						odoo_bin_path = path;
+						return get_python_bin_folder();
+					})
+					.then(path => {
+						python_bin_path = path;
+						return call_odoo_scafold(newModuleName!, mdl_path!, odoo_bin_path!, python_bin_path!);
+					})
+					.catch(err => {
+						vscode.window.showInformationMessage("Creación del módulo cancelada.");
+					});
 				inputBox.hide();
 			} else {
 				vscode.window.showInformationMessage("Creación del módulo cancelada.");
@@ -74,8 +172,6 @@ export function activate(context: vscode.ExtensionContext) {
 			}
 		});
 		inputBox.show();
-
-
 
 
 
