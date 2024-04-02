b9d1ef82560e0090320aa856d957bf4472c7eaa9
Edgar Tenesaca <edgartenesaca@gmail.com>
2024-04-02 15:54:42 -0500

    Agrgando comando para crear un widget

--- src/extension.ts
+++ src/extension.ts
@@ -3,6 +3,7 @@
 import * as vscode from 'vscode';
 import * as child_process from 'child_process';
 import * as path from 'path';
+import * as fs from 'fs';
 
 function call_odoo_scafold(
 	mdl: string,
@@ -85,6 +86,7 @@ function get_odoo_bin_folder(): Promise<string> {
 	});
 }
 
+
 function get_python_bin_folder(): Promise<string> {
 	return new Promise((resolve, reject) => {
 		const config = vscode.workspace.getConfiguration('odoo-awesome');
@@ -114,39 +116,113 @@ function get_python_bin_folder(): Promise<string> {
 }
 
 
+function lsDirectory(currentDir: string): Promise<string[]> {
+	return new Promise((resolve, reject) => {
+		// Listar los archivos en el directorio
+		fs.readdir(currentDir, (err, files) => {
+			if (err) {
+				vscode.window.showErrorMessage(`Error al leer el directorio: ${err.message}`);
+				return reject(err);
+			}
+
+			// Mostrar los archivos en la consola
+			console.log('Archivos en el directorio:');
+			files.forEach(file => {
+				console.log(file);
+			});
+			resolve(files);
+		});
+	});
+}
+
+function getModuleFolder(): Promise<string> {
+	return new Promise((resolve, reject) => {
+		// Obtener el editor de texto activo
+		const editor = vscode.window.activeTextEditor;
+		if (!editor) {
+			reject('No seleccionó ningun archvo');
+		}
+		let finded = false;
+
+		let cur_folder = editor!.document.uri.fsPath;
+		let prev_folder = cur_folder;
+		while (!finded) {
+			cur_folder = path.dirname(cur_folder);
+			if (path.basename(cur_folder) === 'addons') {
+				// revisar si la carpeta contiene init.py y al capeta wizard
+				lsDirectory(cur_folder)
+					.then(files => {
+						let x = 19;
+						x = 19;
+						x = 19;
+						resolve(prev_folder);
+					})
+					.catch(err => reject(err));
+			}
+			prev_folder = cur_folder;
+		}
+		resolve(cur_folder);
+	});
+}
+
+
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
 
-	const config = vscode.workspace.getConfiguration('odoo-awesome');
-
-	// También puedes escuchar cambios en las preferencias
-	const disposables = vscode.workspace.onDidChangeConfiguration(event => {
-		if (event.affectsConfiguration('odoo-awesome.odoo-bin-path')) {
-			const newValue = config.get('odoo-bin-path');
-			console.log('La preferencia odoo-awesome.odoo-bin-path ha cambiado:', newValue);
-			vscode.window.showErrorMessage("Creación del módulo cancelada.");
-		}
-	});
-
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
 	// This line of code will only be executed once when your extension is activated
 	console.log('Congratulations, your extension "odoo-awesome" is now active!');
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('odoo-new-module', () => {
+	//====================================
+	//OERP 7
+	//====================================
+	let oerp7_new_wizard = vscode.commands.registerCommand('oerp7_new_wizard', () => {
+		let wiz_name: string | undefined;
+		let wiz_path: string | undefined;
+
+		const inputBox = vscode.window.createInputBox();
+		inputBox.title = "Nuevo wizard";
+		inputBox.placeholder = "Nombre del wizard";
+		inputBox.onDidChangeValue(value => {
+			if (value.includes(' ')) {
+				inputBox.validationMessage = 'No puede contener espacios';
+			} else {
+				inputBox.validationMessage = '';
+			}
+			wiz_name = value.trim().toLowerCase().replaceAll(' ', '_');
+
+		});
+		inputBox.onDidAccept(() => {
+			if (wiz_name) {
+				// Armar un nombre con el de la carpeta conedora
+				getModuleFolder().then(folder => {
+					vscode.window.showInformationMessage(`Wizard creado '${wiz_name}'.`);
+				}).catch(err => vscode.window.showErrorMessage("Algo ha salido mal."))
+				inputBox.hide();
+			} else {
+				vscode.window.showErrorMessage("Creación del wizard cancelada.");
+				inputBox.hide();
+			}
+		});
+		inputBox.show();
+	});
+	context.subscriptions.push(oerp7_new_wizard);
+
+	//====================================
+	//ODOO
+	//====================================
+
+	// Comando para crear un nuevo modulo vacío
+	let odoo_new_module = vscode.commands.registerCommand('odoo-new-module', () => {
 		let newModuleName: string | undefined;
 		const inputBox = vscode.window.createInputBox();
 		inputBox.title = "Crear nuevo módulo de odoo";
 		inputBox.placeholder = "Nombre del módulo";
-		inputBox.placeholder = "Nombre del módulo";
 		inputBox.onDidChangeValue(value => {
-			if (value.includes(' ')){
+			if (value.includes(' ')) {
 				inputBox.validationMessage = 'No puede contener espacios';
 			} else {
-				inputBox.validationMessage = ''
+				inputBox.validationMessage = '';
 			}
 			newModuleName = value.trim().toLowerCase().replaceAll(' ', '_');
 		});
@@ -178,12 +254,8 @@ export function activate(context: vscode.ExtensionContext) {
 			}
 		});
 		inputBox.show();
-
-
-
 	});
-
-	context.subscriptions.push(disposable);
+	context.subscriptions.push(odoo_new_module);
 
 }
 
